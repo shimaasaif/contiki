4966fcd91ea512b90ba56b83b745a77230e53e70
shimaasaif <sa114@fayoum.edu.eg>
2014-10-07 18:15:13 +0300

    Update project-conf.h

--- examples/iotlab/03-ipv6-examples/project-conf.h
+++ examples/iotlab/03-ipv6-examples/project-conf.h
@@ -1,7 +1,7 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-#define RF2XX_TX_POWER  PHY_POWER_3dBm
+#define RF2XX_TX_POWER  PHY_POWER_m17dBm
 #define RF2XX_LEDS_ON
 
 #endif /* PROJECT_CONF_H_ */
